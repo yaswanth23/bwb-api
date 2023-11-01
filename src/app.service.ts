9dd0203449be58e387584a422ad51a7ce6524392
yash <yashwanth.k@mooo.farm>
2023-11-01 14:34:23 +0530

    initial setup and ping api

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getPing(): any {
+    return {
+      server_time: `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`,
+      server_name: 'bwb-api',
+      version: '1.0',
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { BigIntSerializerPipe } from './common/pipes/bigIntSerializer.pipe';
+import { BigIntInterceptor } from './common/interceptors/bigInt.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  // enabling swagger only on development
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('BWB API')
+      .setDescription("BWB")
+      .setVersion('1.0')
+      .addTag('BWB Code System')
+      .build();
+    const document = SwaggerModule.createDocument(app, config, {
+      include: [],
+      deepScanRoutes: true,
+      operationIdFactory: (controllerKey: string, methodKey: string) =>
+        methodKey,
+    });
+    SwaggerModule.setup('api', app, document);
+  }
+  app.useGlobalPipes(new ValidationPipe(), new BigIntSerializerPipe());
+  app.useGlobalInterceptors(new BigIntInterceptor());
+  await app.listen(9100);
 }
 bootstrap();
