c79935276822a63305f5e7e7a572fc3a2623e65a
yash <yashwanth.k@mooo.farm>
2023-11-05 22:50:42 +0530

    contact us api changes

--- src/services/misc/misc.service.ts
+++ src/services/misc/misc.service.ts
@@ -7,6 +7,35 @@ export class MiscService {
   constructor(private prismaService: PrismaService) {}
 
   async captureContactUsDetails(contactUsInputDto: ContactUsInputDto) {
-    return contactUsInputDto;
+    const contactRequestData =
+      await this.prismaService.contactrequests.findFirst({
+        where: {
+          emailid: contactUsInputDto.emailId,
+          mobilenumber: contactUsInputDto.mobileNumber.toString(),
+        },
+      });
+
+    if (contactRequestData) {
+      await this.prismaService.contactrequests.update({
+        data: {
+          counter: contactRequestData.counter + 1,
+        },
+        where: {
+          id: contactRequestData.id,
+        },
+      });
+    } else {
+      await this.prismaService.contactrequests.create({
+        data: {
+          emailid: contactUsInputDto.emailId,
+          fullname: contactUsInputDto.fullname,
+          organisationname: contactUsInputDto.organisationName,
+          mobilenumber: contactUsInputDto.mobileNumber.toString(),
+          counter: 1,
+          createdat: new Date().toISOString(),
+        },
+      });
+    }
+    return { data: { statusCode: 200, message: 'success' } };
   }
 }
